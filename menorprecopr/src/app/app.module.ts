20e36bf5f35383cd95bcaf49e6251dcc981d4bc0
Eduardo Toporoski <edutoporoski@gmail.com>
2018-10-18 23:38:29 -0300

    Tutorial 2-Display a Heroes List

--- menorprecopr/src/app/app.module.ts
+++ menorprecopr/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
